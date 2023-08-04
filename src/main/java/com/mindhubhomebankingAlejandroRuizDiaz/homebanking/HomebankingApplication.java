34f2c0f42e180b3635ea1a01d7f4b91971c4f372
Alejandro Ruiz Diaz <ajrd21z@gmail.com>
2023-08-04 12:16:53 -0300

    Se añadio las clase Client.java y la interfaz ClientRepository.java.

--- src/main/java/com/mindhubhomebankingAlejandroRuizDiaz/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhubhomebankingAlejandroRuizDiaz/homebanking/HomebankingApplication.java
@@ -1,13 +1,25 @@
 package com.mindhubhomebankingAlejandroRuizDiaz.homebanking;
 
+import com.mindhubhomebankingAlejandroRuizDiaz.homebanking.models.Client;
+import com.mindhubhomebankingAlejandroRuizDiaz.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return args -> {
+			Client client = new Client("44884979", "Alejandro", "Ruiz Diaz", "pepito@pepito.com");
+			clientRepository.save(client);
+		};
 	}
 
 }
---
+++ src/main/java/com/mindhubhomebankingAlejandroRuizDiaz/homebanking/models/Client.java
@@ -0,0 +1,67 @@
+package com.mindhubhomebankingAlejandroRuizDiaz.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String dni;
+    private String name;
+    private String lastname;
+    private String email;
+
+    public Client(){
+
+    }
+
+    public Client(String dni, String name, String lastname, String email) {
+        this.dni = dni;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
